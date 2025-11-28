e8675dee46f0179cfb0589f693db01f0b3aed7a4
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-11-28 05:53:01 +0100

    feat: updated

--- src/app/(home)/layout.jsx
+++ src/app/(home)/layout.jsx
@@ -6,7 +6,7 @@ export default function HomeLayout({ children }) {
   return (
     <>
       <Header />
-      <div className="container mx-auto px-2 sm:px-4 lg:px-6 my-2">
+      <div className="container mx-auto px-2 sm:px-4 lg:px-6 my-2 overflow-x-hidden!">
         {children}
       </div>
       <Footer />
